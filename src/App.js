841f7a9fc48ec78a7e1bbf7054421c1d4535c08b
debojyoti <debojyoti.js@gmail.com>
2019-11-03 15:06:16 +0530

    Modified App.js with parent route structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store";
 import { Provider } from "react-redux";
-import HomePage from './pages/home-page';
-import ProtectedRoute from './components/protected-routes';
-import LoginPage from './pages/login-page';
-import FullPageLoader from "./containers/full-page-loader";
-import { ToastsContainer, ToastsStore, ToastsContainerPosition } from "react-toasts";
+import {
+  ToastsContainer,
+  ToastsStore,
+  ToastsContainerPosition
+} from "react-toasts";
+import Main from "./containers/main";
 
 function App() {
   return (
@@ -15,11 +16,12 @@ function App() {
       <PersistGate loading={<div></div>} persistor={persistor}>
         <Router>
           <div>
-            <FullPageLoader></FullPageLoader>
-            <ToastsContainer store={ToastsStore} position={ToastsContainerPosition.BOTTOM_RIGHT} />
+            <ToastsContainer
+              store={ToastsStore}
+              position={ToastsContainerPosition.BOTTOM_RIGHT}
+            />
             <Switch>
-              <Route exact path="/login" component={LoginPage} />
-              <ProtectedRoute exact path="*" component={HomePage} redirectRoute="/login" /> */}
+              <Route exact path="*" component={Main} />
             </Switch>
           </div>
         </Router>
