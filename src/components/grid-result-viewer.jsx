1af59ee3ce4601870ca74af0b43334daac72523e
debojyoti <debojyoti.js@gmail.com>
2019-11-04 00:40:43 +0530

    Added infinite scroll support

--- src/components/grid-result-viewer.jsx
+++ src/components/grid-result-viewer.jsx
@@ -1,213 +1,95 @@
 import React, { Component } from "react";
 import ExpandedGifViewer from "./expanded-gif-viewer";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { showToast } from "../helper-methods/index";
 import SearchInProgress from "./search-in-progress";
+import InfiniteScroll from 'react-infinite-scroller';
 
 class GridResultViewer extends Component {
   state = {};
 
+  _showCopySuccessMessage = () => {
+    console.log("1 :", 1);
+    showToast("Copied to your clipboard!", "success");
+  };
+
   render() {
+    const { gifs, pagination, canLoadMore, loadMore } = this.props;
+    console.log('gifs :', gifs);
     return (
       <React.Fragment>
         <div id="result-grid-wrapper">
-          <SearchInProgress />
+          {/* <SearchInProgress /> */}
           <div id="results-wrapper" className="fColumn aIC jCC">
             <div id="result-meta-data-wrapper" className="fRow aIC jCS">
-              <h5>GIFs found: 12310</h5>
+              <h5>
+                GIFs found:{" "}
+                {pagination && pagination.total_count
+                  ? pagination.total_count
+                  : 0}
+              </h5>
             </div>
-            <div id="gif-result-grid" className="fRow aIC jCC">
-              <div className="gif-result">
-                <div className="gif-thumbnail"></div>
-                <div className="gif-quick-actions fRow aIC jCSB">
-                  <div className="social-links fRow aIC jCE">
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-instagram" aria-hidden="true"></i>
-                    </div>
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-facebook" aria-hidden="true"></i>
-                    </div>
-                  </div>
-                  <div className="quick-action  fRow aIC jCC">
-                    <i className="fa fa-link" aria-hidden="true"></i>
-                  </div>
-                </div>
+
+            <InfiniteScroll
+              pageStart={0}
+              loadMore={loadMore}
+              hasMore={canLoadMore}
+              threshold={250}
+              loader={<SearchInProgress isVisible={true} key={0} />}
+            >
+              <div id="gif-result-grid" className="fRow aIC jCC">
+                {gifs && gifs.length
+                  ? gifs.map((gif, gifIndex) => (
+                      <div className="gif-result" key={gifIndex}>
+                        <div
+                          className="gif-thumbnail"
+                          style={{
+                            backgroundImage: `url(${gif.images.preview_gif.url})`
+                          }}
+                        ></div>
+                        <div className="gif-quick-actions fRow aIC jCSB">
+                          <div className="social-links fRow aIC jCE">
+                            <a
+                              href={`https://giphy.com/login/twitter?next=https://giphy.com/login/twitter/finalize?next=${gif.url}`}
+                              target="_blank"
+                              style={{ textDecoration: "none" }}
+                            >
+                              <div className="quick-action  fRow aIC jCC">
+                                <i
+                                  className="fa fa-twitter"
+                                  aria-hidden="true"
+                                ></i>
+                              </div>
+                            </a>
+                            <a
+                              href={`http://www.facebook.com/sharer/sharer.php?u=${gif.url}`}
+                              target="_blank"
+                              style={{ textDecoration: "none" }}
+                            >
+                              <div className="quick-action fRow aIC jCC">
+                                <i
+                                  className="fa fa-facebook"
+                                  aria-hidden="true"
+                                ></i>
+                              </div>
+                            </a>
+                          </div>
+                          <CopyToClipboard
+                            text={gif.url || ""}
+                            onCopy={() => this._showCopySuccessMessage()}
+                          >
+                            <div className="quick-action  fRow aIC jCC">
+                              <i className="fa fa-link" aria-hidden="true"></i>
+                            </div>
+                          </CopyToClipboard>
+                        </div>
+                      </div>
+                    ))
+                  : null}
               </div>
-              <div className="gif-result">
-                <div className="gif-thumbnail"></div>
-                <div className="gif-quick-actions fRow aIC jCSB">
-                  <div className="social-links fRow aIC jCE">
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-instagram" aria-hidden="true"></i>
-                    </div>
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-facebook" aria-hidden="true"></i>
-                    </div>
-                  </div>
-                  <div className="quick-action  fRow aIC jCC">
-                    <i className="fa fa-link" aria-hidden="true"></i>
-                  </div>
-                </div>
-              </div>
-              <div className="gif-result">
-                <div className="gif-thumbnail"></div>
-                <div className="gif-quick-actions fRow aIC jCSB">
-                  <div className="social-links fRow aIC jCE">
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-instagram" aria-hidden="true"></i>
-                    </div>
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-facebook" aria-hidden="true"></i>
-                    </div>
-                  </div>
-                  <div className="quick-action  fRow aIC jCC">
-                    <i className="fa fa-link" aria-hidden="true"></i>
-                  </div>
-                </div>
-              </div>
-              <div className="gif-result">
-                <div className="gif-thumbnail"></div>
-                <div className="gif-quick-actions fRow aIC jCSB">
-                  <div className="social-links fRow aIC jCE">
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-instagram" aria-hidden="true"></i>
-                    </div>
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-facebook" aria-hidden="true"></i>
-                    </div>
-                  </div>
-                  <div className="quick-action  fRow aIC jCC">
-                    <i className="fa fa-link" aria-hidden="true"></i>
-                  </div>
-                </div>
-              </div>
-              <div className="gif-result">
-                <div className="gif-thumbnail"></div>
-                <div className="gif-quick-actions fRow aIC jCSB">
-                  <div className="social-links fRow aIC jCE">
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-instagram" aria-hidden="true"></i>
-                    </div>
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-facebook" aria-hidden="true"></i>
-                    </div>
-                  </div>
-                  <div className="quick-action  fRow aIC jCC">
-                    <i className="fa fa-link" aria-hidden="true"></i>
-                  </div>
-                </div>
-              </div>
-              <div className="gif-result">
-                <div className="gif-thumbnail"></div>
-                <div className="gif-quick-actions fRow aIC jCSB">
-                  <div className="social-links fRow aIC jCE">
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-instagram" aria-hidden="true"></i>
-                    </div>
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-facebook" aria-hidden="true"></i>
-                    </div>
-                  </div>
-                  <div className="quick-action  fRow aIC jCC">
-                    <i className="fa fa-link" aria-hidden="true"></i>
-                  </div>
-                </div>
-              </div>
-              <div className="gif-result">
-                <div className="gif-thumbnail"></div>
-                <div className="gif-quick-actions fRow aIC jCSB">
-                  <div className="social-links fRow aIC jCE">
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-instagram" aria-hidden="true"></i>
-                    </div>
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-facebook" aria-hidden="true"></i>
-                    </div>
-                  </div>
-                  <div className="quick-action  fRow aIC jCC">
-                    <i className="fa fa-link" aria-hidden="true"></i>
-                  </div>
-                </div>
-              </div>
-              <div className="gif-result">
-                <div className="gif-thumbnail"></div>
-                <div className="gif-quick-actions fRow aIC jCSB">
-                  <div className="social-links fRow aIC jCE">
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-instagram" aria-hidden="true"></i>
-                    </div>
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-facebook" aria-hidden="true"></i>
-                    </div>
-                  </div>
-                  <div className="quick-action  fRow aIC jCC">
-                    <i className="fa fa-link" aria-hidden="true"></i>
-                  </div>
-                </div>
-              </div>
-              <div className="gif-result">
-                <div className="gif-thumbnail"></div>
-                <div className="gif-quick-actions fRow aIC jCSB">
-                  <div className="social-links fRow aIC jCE">
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-instagram" aria-hidden="true"></i>
-                    </div>
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-facebook" aria-hidden="true"></i>
-                    </div>
-                  </div>
-                  <div className="quick-action  fRow aIC jCC">
-                    <i className="fa fa-link" aria-hidden="true"></i>
-                  </div>
-                </div>
-              </div>
-              <div className="gif-result">
-                <div className="gif-thumbnail"></div>
-                <div className="gif-quick-actions fRow aIC jCSB">
-                  <div className="social-links fRow aIC jCE">
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-instagram" aria-hidden="true"></i>
-                    </div>
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-facebook" aria-hidden="true"></i>
-                    </div>
-                  </div>
-                  <div className="quick-action  fRow aIC jCC">
-                    <i className="fa fa-link" aria-hidden="true"></i>
-                  </div>
-                </div>
-              </div>
-              <div className="gif-result">
-                <div className="gif-thumbnail"></div>
-                <div className="gif-quick-actions fRow aIC jCSB">
-                  <div className="social-links fRow aIC jCE">
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-instagram" aria-hidden="true"></i>
-                    </div>
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-facebook" aria-hidden="true"></i>
-                    </div>
-                  </div>
-                  <div className="quick-action  fRow aIC jCC">
-                    <i className="fa fa-link" aria-hidden="true"></i>
-                  </div>
-                </div>
-              </div>
-              <div className="gif-result">
-                <div className="gif-thumbnail"></div>
-                <div className="gif-quick-actions fRow aIC jCSB">
-                  <div className="social-links fRow aIC jCE">
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-instagram" aria-hidden="true"></i>
-                    </div>
-                    <div className="quick-action  fRow aIC jCC">
-                      <i className="fa fa-facebook" aria-hidden="true"></i>
-                    </div>
-                  </div>
-                  <div className="quick-action  fRow aIC jCC">
-                    <i className="fa fa-link" aria-hidden="true"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
+            </InfiniteScroll>
+
+            {/*  */}
           </div>
         </div>
 
--- src/pages/featured-gifs-page.jsx
+++ src/pages/featured-gifs-page.jsx
@@ -1,9 +1,73 @@
 import React, { Component } from "react";
 import GridResultViewer from '../components/grid-result-viewer';
+import { deepClone, showToast } from "../helper-methods";
+import { fetchFeaturedGifs } from '../http-calls/index';
+import { GIF_LIMIT_PER_LOAD } from '../config/index';
+import SearchInProgress from "../components/search-in-progress";
 
 class FeaturedPage extends Component {
-  state = {};
+  state = {
+    gifs: [],
+    pagination: {},
+    isSearchActive: false,
+    isLoaderActive: false
+  };
+
+  async componentDidMount() {
+    await this._initialLoad();
+  }
+
+  _initialLoad = () => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await this._activateLoader(true);
+        const gifResponse = await fetchFeaturedGifs(GIF_LIMIT_PER_LOAD, 0);
+        await this._updateStateWithNewGifs(gifResponse);
+        await this._activateLoader(false);
+      } catch (err) {
+        showToast("Server error", "error");
+      }
+    });
+  };
+
+  _activateLoader = shouldActivate => {
+    return new Promise((resolve, reject) => {
+      this.setState({ isLoaderActive: shouldActivate }, () => {
+        resolve();
+      });
+    });
+  };
+
+  _updateStateWithNewGifs = gifResponse => {
+    return new Promise((resolve, reject) => {
+      let { gifs, pagination } = deepClone(this.state);
+      if (gifResponse.pagination.count > 0) {
+        // Append gifs
+        gifs = [ ...gifs, ...gifResponse.data ];
+        // Update pagination
+        pagination = gifResponse.pagination;
+      }
+      this.setState({ gifs, pagination }, () => {
+        resolve();
+      });
+    });
+  };
+
+  _loadMoreGifsIfAvailable = async () => {
+    const { pagination } = deepClone(this.state);
+    if (this._checkIfCanLoadMore()) {
+      const gifResponse = await fetchFeaturedGifs(GIF_LIMIT_PER_LOAD, pagination.offset+GIF_LIMIT_PER_LOAD);
+      await this._updateStateWithNewGifs(gifResponse);
+    }
+  };
+
+  _checkIfCanLoadMore = () => {
+    const {pagination} = this.state;
+    return pagination.total_count - (pagination.offset+1) > 0;
+  }
+  
   render() {
+    const { isLoaderActive, gifs, pagination } = this.state;
     return (
       <div id="result-mode" className="fColumn jCC aIC">
         <div id="result-header" className="fRow jCC aIC">
@@ -11,11 +75,17 @@ class FeaturedPage extends Component {
             src="runtime_assets/images/featured-star-transparent.gif"
             alt=""
             id="featured-icon"
-            style={{width: 70}}
+            style={{ width: 70 }}
           />
           <h3>Featured gifs</h3>
         </div>
-        <GridResultViewer />
+        <GridResultViewer 
+          gifs={gifs} 
+          pagination={pagination}
+          canLoadMore={this._checkIfCanLoadMore()}
+          loadMore={ this._loadMoreGifsIfAvailable } 
+        />
+        <SearchInProgress isVisible={isLoaderActive} />
       </div>
     );
   }
