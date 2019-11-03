140c6351d48d5b48dbb259b898d81cdd9b08a626
debojyoti <debojyoti.js@gmail.com>
2019-11-03 18:19:41 +0530

    Added link featured page in idle page

--- src/pages/idle-page.jsx
+++ src/pages/idle-page.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class IdlePage extends Component {
   state = {};
+
   render() {
     return (
       <div id="idle-mode" className="fColumn jCC aIC">
         <div id="landing-bot"></div>
         <h5>
           Try searching something or browse{" "}
+          <Link to="/featured" >
           <span id="featured">featured gifs</span>!
+          </Link>
         </h5>
       </div>
     );
