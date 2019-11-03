045d22d7942521025a886e96f9c1c554bcd37542
debojyoti <debojyoti.js@gmail.com>
2019-11-03 22:30:46 +0530

    Trimmed out error handler boilerplate code

--- src/error-handler/index.js
+++ src/error-handler/index.js
@@ -3,8 +3,7 @@ import { logout } from "../helper-methods";
 export const handleErrorIfAvailable = httpResponse => {
     switch(httpResponse.status) {
         case 401: {
-            // Token expired
-            logout();
+            
         }
     }
 } 
